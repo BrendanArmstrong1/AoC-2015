189268bb7af07460b04957d2ce6ef2927f6fae1b
Brendan Armstrong <Brendan.Armstrong@mail.com>
2022-03-27 16:28:20 -0400

    completed day 2!!

--- dec-2/src/main.rs
+++ dec-2/src/main.rs
@@ -2,13 +2,13 @@ use std::fs;
 
 fn main() {
     let file = fs::read_to_string("../input.txt").unwrap();
-    part1(&file);
+    // _part1(&file);
+    part2(&file);
 }
 
-fn part1(file: &str) {
+fn _part1(file: &str) {
     let mut total: u32 = 0;
     for line in file.lines() {
-        println!("{}", line);
         let dim: Vec<&str> = line.split('x').collect();
         let len = dim[0].parse::<u32>().unwrap();
         let wid = dim[1].parse::<u32>().unwrap();
@@ -17,7 +17,35 @@ fn part1(file: &str) {
         let s2 = hig * wid;
         let s3 = len * hig;
         let small = vec![s1, s2, s3];
-        total += 2*s1 + 2*s2 + 2*s3 + small.iter().min().unwrap();
+        total += 2 * s1 + 2 * s2 + 2 * s3 + small.iter().min().unwrap();
+    }
+    println!("{}", total);
+}
+
+fn part2(file: &str) {
+    let mut total: u32 = 0;
+    for line in file.lines() {
+        let mut length: u32 = 0;
+        let dim: Vec<&str> = line.split('x').collect();
+        let len = dim[0].parse::<u32>().unwrap();
+        let wid = dim[1].parse::<u32>().unwrap();
+        let hig = dim[2].parse::<u32>().unwrap();
+        length += len * wid * hig;
+        let collection = vec![len, wid, hig];
+        let biggest = collection.iter().max().unwrap();
+        let mut added: u8 = 0;
+        for size in collection.iter() {
+            if size < biggest {
+                println!("line is {}, biggest is {}, size is {}", line, biggest, size);
+                added += 1;
+                length += 2 * size;
+            }
+        }
+        while added < 2 {
+            length += 2 * biggest;
+            added += 1;
+        }
+        total += length;
     }
     println!("{}", total);
 }
