5b7f867272619d45db54387722430ed212c8902b
Brendan Armstrong <Brendan.Armstrong@mail.com>
2022-03-27 14:49:43 -0400

    not the most elegant solution, but part 1 solved

--- dec-2/src/main.rs
+++ dec-2/src/main.rs
@@ -6,7 +6,18 @@ fn main() {
 }
 
 fn part1(file: &str) {
-    for i in file.lines() {
-        print!("{}", i);
+    let mut total: u32 = 0;
+    for line in file.lines() {
+        println!("{}", line);
+        let dim: Vec<&str> = line.split('x').collect();
+        let len = dim[0].parse::<u32>().unwrap();
+        let wid = dim[1].parse::<u32>().unwrap();
+        let hig = dim[2].parse::<u32>().unwrap();
+        let s1 = len * wid;
+        let s2 = hig * wid;
+        let s3 = len * hig;
+        let small = vec![s1, s2, s3];
+        total += 2*s1 + 2*s2 + 2*s3 + small.iter().min().unwrap();
     }
+    println!("{}", total);
 }
