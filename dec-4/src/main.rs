5bb0498ed153f98ae42570a332bfd13c3ce7dfda
Brendan Armstrong <Brendan.Armstrong@mail.com>
2022-03-28 17:31:06 -0400

    easy peasy. just had to modify my code a little

--- dec-4/src/main.rs
+++ dec-4/src/main.rs
@@ -2,10 +2,11 @@ use std::{fs, time::Instant};
 
 fn main() {
     let file = fs::read_to_string("../input.txt").unwrap();
-    part1(&file);
+    // part1(&file);
+    part2(&file);
 }
 
-fn part1(file: &str) {
+fn _part1(file: &str) {
     let now = Instant::now();
     let mut hashed = 0;
     let file = file.trim();
@@ -22,6 +23,27 @@ fn part1(file: &str) {
             } else {
                 count += 1;
             }
+        } else {
+            count += 1;
+        }
+    }
+
+    println!("{} after {}", count, now.elapsed().as_millis());
+}
+
+fn part2(file: &str) {
+    let now = Instant::now();
+    let mut hashed = 0;
+    let file = file.trim();
+    // let file = "abcdef";
+    let mut count = 0;
+    while hashed == 0 {
+        let ctx = format!("{}{}", file, count);
+        let digest = md5::compute(&ctx);
+        let five = &digest[0..3];
+        if five.iter().all(|&x| x == 0_u8) {
+            hashed += 1;
+            println!("{}  {:?}", ctx, digest);
         } else {
             count += 1;
         }
