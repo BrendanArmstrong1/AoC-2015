bc97f0b080221e089ef7c99c2fe1561df550c698
Brendan Armstrong <Brendan.Armstrong@mail.com>
2022-03-28 17:22:38 -0400

    completed part one of day 4

---
+++ dec-4/src/main.rs
@@ -0,0 +1,31 @@
+use std::{fs, time::Instant};
+
+fn main() {
+    let file = fs::read_to_string("../input.txt").unwrap();
+    part1(&file);
+}
+
+fn part1(file: &str) {
+    let now = Instant::now();
+    let mut hashed = 0;
+    let file = file.trim();
+    // let file = "abcdef";
+    let mut count = 0;
+    while hashed == 0 {
+        let ctx = format!("{}{}", file, count);
+        let digest = md5::compute(&ctx);
+        let five = &digest[0..2];
+        if five.iter().all(|&x| x == 0_u8) {
+            if digest[2] < 16 {
+                hashed += 1;
+                println!("{}  {:?}", ctx, digest);
+            } else {
+                count += 1;
+            }
+        } else {
+            count += 1;
+        }
+    }
+
+    println!("{} after {}", count, now.elapsed().as_millis());
+}
